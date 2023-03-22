5a34b705f22e13ac559bba0c22586670a8446b6d
sarintoxic <124868065+vodkaholik@users.noreply.github.com>
2023-03-22 16:39:34 +0300

    k
    m

--- config/a2u.js
+++ config/a2u.js
@@ -2,14 +2,13 @@
 import pi_backend_1 from "pi-backend";
 import dotenv_1 from "dotenv";
 dotenv_1.config();
-const apiKey = process.env.PI_API_KEY ;
+const apiKey = process.env.PI_API_KEY;
 const walletPrivateSeed = process.env.WALLET_PRIVATE_SEED ;
 const pi = new pi_backend_1["default"](apiKey, walletPrivateSeed);
-export async function createWithdraw(payment) {
+export async function createWithdraw(amount, userUid) {
   try {
-    const paymentData=payment;
-    const tt= await pi.createPayment(paymentData)
-    return tt;
+    const res= await pi.createPayment({amount: amount, memo: "WithDraw", metadata: {withdraw: "UserWithdraw"},  uid: userUid })
+    return res;
   }
     catch(err) {
       console.log("Error 1: ", err); 
